605c06fc63673256e6adc4b3c19bcec8a3405e16
ProgRibeiro <119256851+ProgRibeiro@users.noreply.github.com>
2024-03-10 15:57:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Testando as funções
+console.log(multiplicacao(5, 3)); 
+console.log(saudacao("João")); 
